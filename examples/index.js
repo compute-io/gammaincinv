bcecfa9018d7550981c58793f6f7948b8f1f8c18
Planeshifter <pburckhardt@outlook.com>
2015-07-08 23:04:56 +0200

    [UPDATE] examples, package.json

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,83 @@
 'use strict';
 
-var foo = require( './../lib' );
+var matrix = require( 'dstructs-matrix' ),
+	gammaincinv = require( './../lib' );
+
+var data,
+	mat,
+	out,
+	tmp,
+	i;
+
+// ----
+// Plain arrays...
+data = new Array( 100 );
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.random();
+}
+out = gammaincinv( data, 1 );
+console.log( 'Arrays: %s\n', out );
+
+
+// ----
+// Object arrays (accessors)...
+function getValue( d ) {
+	return d.x;
+}
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': data[ i ]
+	};
+}
+out = gammaincinv( data, 1, {
+	'accessor': getValue
+});
+console.log( 'Accessors: %s\n', out );
+
+
+// ----
+// Deep set arrays...
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': [ i, data[ i ].x ]
+	};
+}
+out = gammaincinv( data, 1, {
+	'path': 'x/1',
+	'sep': '/'
+});
+console.log( 'Deepset:' );
+console.dir( out );
+console.log( '\n' );
+
+
+// ----
+// Typed arrays...
+data = new Float64Array( 100 );
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.random();
+}
+tmp = gammaincinv( data, 1 );
+out = '';
+for ( i = 0; i < data.length; i++ ) {
+	out += tmp[ i ];
+	if ( i < data.length-1 ) {
+		out += ',';
+	}
+}
+console.log( 'Typed arrays: %s\n', out );
+
+
+// ----
+// Matrices...
+mat = matrix( data, [10,10], 'float64' );
+out = gammaincinv( mat, 1 );
+console.log( 'Matrix: %s\n', out.toString() );
+
+
+// ----
+// Matrices (custom output data type)...
+out = gammaincinv( mat, 1, {
+	'dtype': 'float32'
+});
+console.log( 'Matrix (%s): %s\n', out.dtype, out.toString() );
